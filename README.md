fa02fc1475039d3fde937e23a7eef7a8172b09e3
WahAj <154734596+wahaj906@users.noreply.github.com>
2024-08-17 14:43:40 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello! THIS IS WAHAJ ALI
+#My First React Project
+
+In this project i have write my name using react
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Wahaj Ali</h1>
+  </StrictMode>,
+)
